e0bb302a632103f360fb394b44dc66c752a89c56
Phat Ky <kyp0717@gmail.com>
2025-01-21 08:41:02 -0500

    add function get_history_bar

--- strategy/simple/simple.py
+++ strategy/simple/simple.py
@@ -1,9 +1,5 @@
 from alpaca_trade_api.rest import REST, TimeFrame
 from datetime import datetime
-api = REST()
 
-start_time = datetime(2021, 6, 8, 9, 32)
-end_time = datetime(2021, 6, 8, 9, 33)
-minute_period = TimeFrame.Minute
 
-api.get_bars("AAPL", minute_period, start_time, end_time, adjustment='raw').df
+
