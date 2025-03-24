b2bcc310e30d1e3d37455e6e2e6fed8d454bd51c
Phage Ky <kyp0717@gmail.com>
2025-03-23 20:23:48 -0400

    add tws test.py

--- api/tws/test.py
+++ api/tws/test.py
@@ -1,3 +1,26 @@
+import threading
+import time
+
 from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
 
 
+class TestApp(EClient, EWrapper):
+    def __init__(self):
+        EClient.__init__(self, self)
+
+    def nextValidId(self, orderId):
+        self.orderId = orderId
+
+    def nextId(self):
+        self.orderId += 1
+        return self.orderId
+
+
+app = TestApp()
+app.connect("127.0.0.1", 7500, 0)
+threading.Thread(target=app.run).start()
+time.sleep(1)
+
+for i in range(0, 5):
+    print(app.nextId())
