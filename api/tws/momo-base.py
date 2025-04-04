f45d33b8415956cbe176e35bbd903e2c39138b41
Phage Ky <kyp0717@gmail.com>
2025-04-04 07:05:13 -0400

    momo base 2

--- api/tws/momo-base.py
+++ api/tws/momo-base.py
@@ -1,20 +1,41 @@
+import threading
+import time
 
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
-import datetime
-import time
-import threading
 
 port = 7500
-host = 127.0.0.1
 
 bank = {}
 position = {}
 
-class Momo(EWrapper,EClient):
+
+class Momo(EWrapper, EClient):
     def __init__(self):
-        EClient.__init__(self,self)
+        EClient.__init__(self, self)
 
     def nextValidId(self, orderId):
         self.orderId = orderId
 
+    def nextId(self):
+        self.orderId += 1
+        return self.orderId
+
+    def error(self, reqId, errorCode, errorString, advanceOrderReject):
+        message = (
+            f"-- ReqId, {reqId} --\n"
+            f"ErrorCode: {errorCode}, ErrorString: {errorString} \n"
+            f"Order Reject: {advanceOrderReject} \n"
+        )
+
+        print(message)
+
+
+app = Momo()
+app.connect("localhost", port, 1005)
+threading.Thread(target=app.run).start
+time.sleep(1)
+
+# Account Updates set to true enable streaming of changes to account info
+# such as position and pnl
+app.reqAccountUpdates(True, "")
