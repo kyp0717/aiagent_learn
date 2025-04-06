45d814815baaca815dde595c4824683e3f7e01da
Phage Ky <kyp0717@gmail.com>
2025-04-06 09:34:59 -0400

    add code

--- api/tws/algo/tws-top-change-scan.py
+++ api/tws/algo/tws-top-change-scan.py
@@ -1,8 +1,10 @@
+import threading
 from datetime import date
 
-from ibapi.client import EClient
+from ibapi.client import EClient, ScannerSubscription
 from ibapi.tag_value import TagValue
 from ibapi.wrapper import EWrapper
+from ibapi.
 
 port = 7497
 global globalDict
@@ -15,6 +17,82 @@ clientId = 1001
 class TestApp(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
+
+    def nextValidId(self, orderId):
+        self.orderId = orderId
+
+    def nextId(self):
+        self.orderId += 1
+        return self.orderId
+
+    def error(self, reqId, errorCode, errorString, advanceOrderReject):
+        message = (
+            f"-- ReqId, {reqId} --\n"
+            f"ErrorCode: {errorCode}, ErrorString: {errorString} \n"
+            f"Order Reject: {advanceOrderReject} \n"
+        )
+        print(message)
+
+    def updateAccountValue(self, key, val, currency, accountName):
+        if key == "TotalCashBalance" and currency == "BASE":
+            bank[key] = float(val)
+
+        # if acct drop below $100k disconnect
+        if float(val) < 100000:
+            self.disconnect
+
+    def position(self, account, contract, position, avgCost):
+        position_ref[contract.symbol] = position
+
+    # Returned Market Scanner information (One rank at a time)
+    def scannerData(
+        self,
+        reqId: int,
+        rank: int,
+        contractDetails: ContractDetails,
+        distance: str,
+        benchmark: str,
+        projection: str,
+        legsStr: str,
+    ):
+        global globalDict
+        globalDict[rank] = [
+            contractDetails.contract,
+            "BID",
+            "ASK",
+            "LAST",
+            "Today",
+            "Yesterday",
+            "today_CHANGE%",
+            "prior_CHANGE%",
+            "today_CHANG",
+        ]
+
+    # End of market scanner
+    def scannerDataEnd(self, reqId: int):
+        # Cancel lingering market scanner
+        self.cancelScannerSubscription(reqId)
+        # Request market data for all of our scanner values.
+        for rank in globalDict:
+            x = threading.Thread(
+                target=self.reqMktData(
+                    reqId=rank,
+                    contract=globalDict[rank][0],
+                    genericTicklist="",
+                    snapshot=True,
+                    regulatorySnapshot=False,
+                )
+            )
+            x.start()
+
+    # Returned market data
+    def tickPrice(
+        self, reqId: TickerId, tickType: TickType, price: float, attrib: TickAttrib
+    ):
+        global globalDict
+        for ind, value in enumerate(globalDict[reqId]):
+            if TickTypeEnum.to_str(tickType) == value:
+                globalDict[reqId][ind] = price
 
 
 def run_loop(app_obj: TestApp):
